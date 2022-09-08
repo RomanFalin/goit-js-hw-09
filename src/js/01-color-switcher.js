8c1f7ee3a5211b3ca4fc2718b5231aeefbe73bb8
RomanFalin <romanfalin01@gmail.com>
2022-09-08 23:59:50 +0200

    task 1 ready / task 2 start

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const onBtn = document.querySelector("button[data-start]");
+const offBtn = document.querySelector("button[data-stop]");
+const body = document.querySelector("body");
+let interval = null;
 
+onBtn.addEventListener("click", () => {
+    interval = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    onBtn.disabled = true;
+    offBtn.disabled = false;
+});
+
+offBtn.addEventListener("click", () => {
+    clearInterval(interval);
+    onBtn.disabled = false;
+    offBtn.disabled = true;
+})
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
